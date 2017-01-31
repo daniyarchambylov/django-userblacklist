e08623270a9b697160872a56117dca89bd4dd551
Daniyar Chambylov <dan.chambylov@gmail.com>
2017-01-31 10:33:55 +0600

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django_userblacklist',
-    version='0.1.0',
+    version='0.1.1',
     packages=['userblacklist', 'userblacklist.migrations'],
     url='https://github.com/daniyarchambylov/django-userblacklist',
     license='',
